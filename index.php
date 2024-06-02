db3d590694eba1cfc01c9d20236bd1d7292ea27f
himikeyy <dustinrafe@gmail.com>
2024-06-02 13:14:14 +0800

    test: Try edit  for collaboration

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Condo Bill</title>
 </head>
+    <?php 
+        echo "Collab Test";
+    ?>
 <body>
     
 </body>
