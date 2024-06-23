4b4963e6cf9f8858477fb81d6d3ec4f3c4ce9832
jirosmokes <dustinrafe@gmail.com>
2024-06-23 19:12:31 +0800

    update: organized files and links

---
+++ admin-side/admin-room-details.php
@@ -0,0 +1,52 @@
+<?php 
+    $rooms = array(
+        array(
+            "room_number" => 101,
+            "room_capacity" => 4
+        ),
+        array(
+            "room_number" => 102,
+            "room_capacity" => 4
+        ),
+        array(
+            "room_number" => 103,
+            "room_capacity" => 4
+        ),
+        array(
+            "room_number" => 104,
+            "room_capacity" => 4
+        ),
+        array(
+            "room_number" => 105,
+            "room_capacity" => 4
+        ),
+        array(
+            "room_number" => 106,
+            "room_capacity" => 4
+        ),
+        array(
+            "room_number" => 107,
+            "room_capacity" => 4
+        ),
+        array(
+            "room_number" => 108,
+            "room_capacity" => 4
+        ),
+        array(
+            "room_number" => 109,
+            "room_capacity" => 4
+        ),
+        array(
+            "room_number" => 110,
+            "room_capacity" => 4
+        ),
+        array(
+            "room_number" => 111,
+            "room_capacity" => 4
+        ),
+        array(
+            "room_number" => 112,
+            "room_capacity" => 4
+        )
+    );
+?>
