a2c1d1c909bcace7443e34a455982e80907ce894
jirosmokes <dustinrafe@gmail.com>
2024-07-08 00:14:43 +0800

    updated: admin dashboard with dynamic revenu graph and paid transaction table

--- admin-side/admin-transaction.php
+++ admin-side/admin-transaction.php
@@ -79,86 +79,8 @@ if (isset($_GET['room_number'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Transaction</title>
     <link rel="stylesheet" href="../admin-style/admin-sidebar.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
-        /* Sidebar styles */
-        .sidebar {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 250px;
-            height: 100%;
-            background: linear-gradient(145deg, #1e1e1e, #2c2c2c);
-            box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
-            transition: all 0.3s;
-            overflow: hidden;
-            display: flex;
-            flex-direction: column;
-        }
-
-        .sidebar header {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            height: 70px;
-            background: #222;
-            box-shadow: 0 1px 5px rgba(0, 0, 0, 0.1);
-        }
-
-        .sidebar a {
-            display: flex;
-            align-items: center;
-            padding: 15px 30px;
-            color: white;
-            text-decoration: none;
-            transition: background-color 0.3s, transform 0.3s;
-        }
-
-        .sidebar a:hover {
-            background-color: rgba(255, 255, 255, 0.1);
-            transform: translateX(5px);
-        }
-
-        .sidebar a.active {
-            background-color: #575757;
-        }
-
-        .sidebar a i {
-            margin-right: 10px;
-        }
-
-        .sidebar form {
-            margin-top: auto; /* Push the form to the bottom */
-        }
-
-        .sidebar form button {
-            background: none;
-            border: none;
-            color: white;
-            cursor: pointer;
-            display: block;
-            font-family: "Open Sans", sans-serif;
-            font-size: 16px;
-            line-height: 65px;
-            padding-left: 30px;
-            text-align: left;
-            transition: all 0.3s ease;
-            width: 100%;
-            box-sizing: border-box;
-        }
-
-        .sidebar form button:hover {
-            background-color: rgba(255, 255, 255, 0.1);
-        }
-
-        .sidebar form button i {
-            margin-right: 10px;
-        }
-
-        .sidebar form button span {
-            letter-spacing: 1px;
-            text-transform: uppercase;
-        }
-
         body {
             font-family: Arial, sans-serif;
             background-color: #202124;
@@ -247,8 +169,8 @@ if (isset($_GET['room_number'])) {
     </style>
 </head>
 <body>
-     <!-- SIDEBAR -->
-     <div class="sidebar">
+      <!-- SIDEBAR -->
+      <div class="sidebar">
         <header><img src="../images/dorm-hub-logo-official.png" alt="" height="30px"></header>
 
         <a href="../admin-side/admin-dashboard.php" class="active" onclick="showContent('dashboard')">
@@ -257,12 +179,12 @@ if (isset($_GET['room_number'])) {
         </a>
 
         <a href="../admin-side/admin-room-selection.php" onclick="showContent('view-tenants')">
-            <i class="fas fa-user-alt"></i>
+            <i class="fa-solid fa-users-viewfinder"></i>
             <span>View Tenants</span>
         </a>
 
         <a href="../admin-side/admin-transaction.php">
-        <i class="fas fa-user-alt"></i>
+        <i class="fas fa-money-bill-transfer"></i>
         <span>Create Bills</span>
          </a>
          
