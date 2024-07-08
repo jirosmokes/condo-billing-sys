d925ee141cfdb18c8799516ac3bbe2247f01e949
unknown <lappaydennis@gmail.com>
2024-07-09 02:40:14 +0800

    Fixed - delete for sql.

--- admin-side/admin-dashboard.php
+++ admin-side/admin-dashboard.php
@@ -4,16 +4,14 @@ if (empty($_SESSION['account_number'])) {
     header("Location: ../landing-page.php");
     exit();
 }
-
 ?>
 
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="images/dorm-hub-logo-official-2.png" type="image/png">
+    <link rel="icon" href="../images/dorm-hub-logo-official-2.png" type="image/png">
 
     <title>DH-Admin</title>
 
@@ -28,16 +26,14 @@ if (empty($_SESSION['account_number'])) {
 
         body {
             margin: 0;
-            font-family: "" Open Sans", sans-serif";
+            font-family: "Open Sans", sans-serif;
             background-color: #1e1e1e;
         }
     </style>
 </head>
-
 <body>
     <div id="content">
         <?php include 'admin-dbContent.php'; ?>
     </div>
 </body>
-
 </html>
